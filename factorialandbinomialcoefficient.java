4ea6f02a49feae3c25ea631f09657f2e0245f7df
akashgn525-afk <akashgn525@gmail.com>
2026-09-12 22:22:37 +0530

    5th commit

---
+++ factorialandbinomialcoefficient.java
@@ -0,0 +1,24 @@
+public class factorialandbinomialcoefficient {
+    static int fact(int n)
+    {
+      int f=1;
+      for(int i=1;i<=n;i++)
+      {
+        f=f*i;
+
+      }
+      return f;
+    }
+    public static int binCoff(int n,int r)
+    {
+      int fact_n=fact(n);
+      int fact_r=fact(r);
+      int fact_n_r=fact(n-r);
+      int binCoff=fact_n/(fact_r*fact_n_r);
+      return binCoff;
+    }                                    
+    public static void main(String args[])
+{
+        
+        System.out.println(binCoff(5,2));
+}}
---
+++ practice.java
@@ -0,0 +1,99 @@
+public class practice {
+    // static int avg(int n)
+    // {
+    //    int sum=0;
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         sum=sum+i;
+    //     }
+    //     int avg=sum/n;
+    //     return avg;
+    // }
+    // public static void main(String args[])
+    // {
+    //     System.out.println(avg(5));
+    // }
+//   static boolean isEven(int n)
+//   {
+//     if(n%2==0)
+// {
+//     return true;
+// }
+//  return false; 
+//  }
+//  public static void main(String args[])
+// {
+//   System.out.println(isEven(100));
+// }
+// public static void main(String args[])
+// {
+// Scanner sc=new Scanner(System.in);
+// System.out.println("enter the number");
+// int n=sc.nextInt();
+// palindrome(n);
+// }
+// static void palindrome(int n)
+// {
+//     int rev=0;
+//     int temp=n;
+//     while(temp>0)
+//     {
+//         int lastDigit=temp%10;
+//         rev=rev*10+lastDigit;
+//         temp=temp/10;
+//     }
+//     if(rev==n)
+//     {
+//         System.out.println("palindrome");
+//     }
+//     else
+//     {
+//         System.out.println("not palindrome");
+//     }
+// }
+//  static int sumOfDigits(int n){
+//     int sum=0;
+//     while(n>0)
+//     {
+//         int lastDigit=n%10;
+//         sum=sum+lastDigit;
+//         n=n/10;
+//     }
+//     return sum;
+// }
+// public static void main(String args[])
+// {
+//     System.out.println(sumOfDigits(153));
+// }
+
+//  static int armstrong(int n){
+//     int sum=0;
+//     while(n>0)
+//     {
+//         int lastDigit=n%10;
+//         sum=sum+lastDigit*lastDigit*lastDigit;
+//         n=n/10;
+//     }
+//     return sum;
+// }
+// public static void main(String args[])
+// {
+//     System.out.println(armstrong(153));
+// }
+
+// public static void lcm(int a,int b)
+// { int x=a;
+//   int y=b;
+//     while(y!=0){
+//         int rem=x%y;
+//         x=y;
+//         y=rem;
+//     }
+//     int lcm=(a*b)/x;
+//     System.out.println(lcm);
+// }
+// public static void main(String args[]){
+//     lcm(20,28);
+// }
+       
+}
